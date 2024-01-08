ee60f1d90f9a1d8eebb37fbe8f29a5268aa0b9d6
YahyobekRahimov <rcoder570@gmail.com>
2024-01-08 17:03:46 +0500

    fixed line under the nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,42 +11,77 @@ export default function Header() {
    const location = useLocation();
 
    return (
-
       <div className="mt-[1.5rem]">
-         <Container classes="flex justify-between items-center mb-[1rem] ">
+         <Container classes="flex justify-between items-center ">
             <div>
                <Logo />
             </div>
             <div>
-               <ul className="list-none flex gap-16">
-                  <li className={`hover:text-green-600 pb-[1em] text-green-700`}>
-                     <NavLink className={`${location.pathname == '/' ? " pb-[1.125rem] border-solid border-b-4 border-b-green-600 text-green-700" : "border-b-white text-black"}`} to="/">Home</NavLink>
+               <ul className="list-none flex gap-16 items-center">
+                  <li
+                     className={`hover:text-green-600 text-green-700`}
+                  >
+                     <NavLink
+                        className={`${
+                           location.pathname == "/"
+                              ? " pb-[1.45rem] border-solid border-b-4 border-b-green-600 text-green-700"
+                              : "border-b-white text-black"
+                        }`}
+                        to="/"
+                     >
+                        Home
+                     </NavLink>
                   </li>
                   <li className="hover:text-green-600">
-                     <NavLink className={`${location.pathname == '/shop' ? " pb-[1.125rem] border-solid border-b-4 border-b-green-600 text-green-700" : "border-b-white text-black"}`} to="/shop">Shop</NavLink>
+                     <NavLink
+                        className={`${
+                           location.pathname == "/shop"
+                              ? " pb-[1.45rem] border-solid border-b-4 border-b-green-600 text-green-700"
+                              : "border-b-white text-black"
+                        }`}
+                        to="/shop"
+                     >
+                        Shop
+                     </NavLink>
                   </li>
                   <li className="hover:text-green-600">
-                     <NavLink className={`${location.pathname == '/plant-care' ? " pb-[1.125rem] border-solid border-b-4 border-b-green-600 text-green-700" : "border-b-white text-black"}`} to="/plant-care">Plant Care</NavLink>
+                     <NavLink
+                        className={`${
+                           location.pathname == "/plant-care"
+                              ? " pb-[1.45rem] border-solid border-b-4 border-b-green-600 text-green-700"
+                              : "border-b-white text-black"
+                        }`}
+                        to="/plant-care"
+                     >
+                        Plant Care
+                     </NavLink>
                   </li>
                   <li className="hover:text-green-600">
-                     <NavLink className={`${location.pathname == '/blogs' ? " pb-[1.125rem] border-solid border-b-4 border-b-green-600 text-green-700" : "border-b-white text-black"}`} to="/blogs">Blogs</NavLink>
+                     <NavLink
+                        className={`${
+                           location.pathname == "/blogs"
+                              ? "pb-[1.45rem] border-solid border-b-4 border-b-green-600 text-green-700"
+                              : "border-b-white text-black"
+                        }`}
+                        to="/blogs"
+                     >
+                        Blogs
+                     </NavLink>
                   </li>
                </ul>
             </div>
             <div className="flex gap-8 items-center">
                <Search />
                <ColorBadge />
-               <button className=" items-center flex gap-[0.25rem] text-white px-[1.0625rem] py-[0.5rem] rounded-md bg-green-600">
+               <button className="items-center flex gap-[0.25rem] text-white px-[1.0625rem] py-[0.5rem] rounded-md bg-green-600">
                   <Login />
                   Login
                </button>
             </div>
-
          </Container>
          <Container>
-            <div className="h-[0.3px] w-full bg-green-200"></div>
+            <div className="h-[0.3px] w-full bg-green-200 mt-4"></div>
          </Container>
       </div>
-
    );
 }
