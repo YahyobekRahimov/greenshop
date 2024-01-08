5efd3aff24d529030dbd7c88265f92fca80a3219
YahyobekRahimov <rcoder570@gmail.com>
2024-01-08 16:53:36 +0500

    added newsletter input in footer, added new variables...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,40 +2,61 @@ import Container from "./Container";
 import Plant from "../images/plant.png";
 import Cactus from "../images/cactus.png";
 import Teapot from "../images/teapot.png";
+import Button from "./Button";
 
 export default function Footer() {
    return (
-      <Container classes="flex bg-slate-100">
-         <div className="flex">
-            <div>
-               <img src={Plant} alt="plant" />
-               <h3 className="text-[1.125rem] font-bold">
-                  Garden Care
-               </h3>
-               <p className="text-[0.875rem]">
-                  We are an online plant shop offering a wide range of
-                  cheap and trendy plants.
-               </p>
+      <Container classes="flex bg-softBackground">
+         <div className="flex py-8">
+            {/* Cards */}
+            <div className="flex w-[65%]">
+               <div>
+                  <img src={Plant} alt="plant" />
+                  <h3 className="text-[1.125rem] font-bold">
+                     Garden Care
+                  </h3>
+                  <p className="text-[0.875rem]">
+                     We are an online plant shop offering a wide range
+                     of cheap and trendy plants.
+                  </p>
+               </div>
+               <div>
+                  <img src={Cactus} alt="plant" />
+                  <h3 className="text-[1.125rem] font-bold">
+                     Plant Renovation
+                  </h3>
+                  <p className="text-[0.875rem]">
+                     We are an online plant shop offering a wide range
+                     of cheap and trendy plants.
+                  </p>
+               </div>
+               <div>
+                  <img src={Teapot} alt="plant" />
+                  <h3 className="text-[1.125rem] font-bold">
+                     Watering Garden
+                  </h3>
+                  <p className="text-[0.875rem]">
+                     We are an online plant shop offering a wide range
+                     of cheap and trendy plants.
+                  </p>
+               </div>
             </div>
-            <div>
-               <img src={Cactus} alt="plant" />
+            <div className="w-[30%]">
                <h3 className="text-[1.125rem] font-bold">
-                  Garden Care
+                  Would you like to join newsletters?
                </h3>
-               <p className="text-[0.875rem]">
-                  We are an online plant shop offering a wide range of
-                  cheap and trendy plants.
-               </p>
-            </div>
-            <div>
-               <img src={Teapot} alt="plant" />
-               <h3 className="text-[1.125rem] font-bold">
-                  Garden Care
-               </h3>
-               <p className="text-[0.875rem]">
-                  We are an online plant shop offering a wide range of
-                  cheap and trendy plants.
-               </p>
+               <div className="flex">
+                  <input
+                     type="email"
+                     name="email"
+                     id="email"
+                     className="w-full p-3 text-[0.875rem] duration-200 border-gray-300 shadow-input focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-300 focus:border-r-0 rounded-l-lg"
+                     placeholder="Enter your email address"
+                  />
+                  <Button classes="bg-primary text-white py-3 px-6 rounded-r-lg hover:bg-primaryDark">
+                     Join
+                  </Button>
+               </div>
             </div>
          </div>
          <div></div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,12 @@ export default {
       extend: {
          boxShadow: {
             primary: "0 4px 6px rgba(0, 0, 0, 0.1)",
+            input: "0px 0px 20px 0px rgba(0, 0, 0, 0.06)",
          },
          colors: {
-           primary:"var(--green-primary)"
+            primary: "var(--green-primary)",
+            primaryDark: "var(--green-primary-darker)",
+            softBackground: "#FBFBFB",
          },
       },
       container: {
