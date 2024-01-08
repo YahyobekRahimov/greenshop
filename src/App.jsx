fb50f4034153d9c9dad899c78e0f90ea49efa68a
YahyobekRahimov <rcoder570@gmail.com>
2024-01-08 11:56:17 +0500

    added myAccount page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-   return (
-      <div className="text-2xl w-[200px] underline hover:bg-slate-200">
-         Hello World!
-      </div>
-   );
+   return <div className="">Hello World!</div>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
    theme: {
-      extend: {},
+      extend: {
+         boxShadow: {
+            primary: "0 4px 6px rgba(0, 0, 0, 0.1)",
+         },
+         colors: {
+            customBlue: "#3498db",
+         },
+      },
    },
    plugins: [],
 };
