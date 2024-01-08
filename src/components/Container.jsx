2fd5d7b1ceb5af0e4a386afdffcd2c3bdde9e5dd
UbaydulloH00 <turonarsloni00@gmail.com>
2024-01-08 14:57:05 +0500

    added  images

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ export default function Container(props) {
    const { children, classes } = props;
    return (
       <div
-         className={`container px-[7.5rem] mx-auto bg-slate-400 ${classes}`}
+         className={`container px-[7.5rem] mx-auto  ${classes}`}
       >
          {children}
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
 import Container from "./Container";
 import { NavLink } from "react-router-dom";
+import Logo from '../img/logo.svg?react'
+import Search from '../img/search.svg?react'
+import ShoppingIcon from '../img/shopping.svg?react'
 
 export default function Header() {
    return (
-      <Container>
-         <ul className="list-none flex gap-5">
+      <Container classes='flex justify-between items-center '>
+         <div>
+            <Logo/>
+         </div>
+         <div>
+            <ul className="list-none flex gap-5">
             <li>
                <NavLink to="/">Home</NavLink>
             </li>
@@ -18,6 +25,11 @@ export default function Header() {
                <NavLink to="/shop/checkout">Checkout</NavLink>
             </li>
          </ul>
+         </div>
+         <div className="flex gap-8">
+            <Search/>
+            <ShoppingIcon/>
+         </div>
       </Container>
    );
 }
