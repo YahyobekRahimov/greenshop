5c67cf976f10ac0e75f07b31fa3a46cf9e02ae95
UbaydulloH00 <turonarsloni00@gmail.com>
2024-01-09 11:50:49 +0500

    modal oyna qoshildi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,20 @@ import Search from "../images/search.svg?react";
 import Login from "../images/Logout.svg?react";
 import ColorBadge from "./ColorBadge";
 import { useLocation } from "react-router-dom";
+import ModalSearch from '../components/modal-search'
+import { useState } from "react";
 
 export default function Header() {
+   function handleclik (e){
+     setModalSearch(true);
+   }
+   const [modalSearch,setModalSearch] = useState(false);
    const location = useLocation();
 
    return (
       <div className="mt-[1.5rem]">
          <Container classes="flex justify-between items-center ">
-            <div>
+            <div className="cursor-pointer">
                <Logo />
             </div>
             <div>
@@ -70,10 +76,11 @@ export default function Header() {
                   </li>
                </ul>
             </div>
-            <div className="flex gap-8 items-center">
-               <Search />
+            <div className="  flex gap-8 items-center">
+               <Search onClick={handleclik} className={`${setModalSearch ? "hidden":" block rounded-full hover:bg-gray-100 cursor-pointer" }`} />
+                 <ModalSearch className={`${setModalSearch ? "hidden":" block cursor-pointer" }`} />
                <ColorBadge />
-               <button className="items-center flex gap-[0.25rem] text-white px-[1.0625rem] py-[0.5rem] rounded-md bg-green-600">
+               <button className=" hover:bg-primaryDark items-center flex gap-[0.25rem] text-white px-[1.0625rem] py-[0.5rem] rounded-md bg-green-600">
                   <Login />
                   Login
                </button>
