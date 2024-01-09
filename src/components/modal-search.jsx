21fcb53ce7d13b6180ed54d1ced406d8358d9368
YahyobekRahimov <rcoder570@gmail.com>
2024-01-09 12:01:54 +0500

    fixed color-related issues in modal

--- src/components/modal-search.jsx
+++ src/components/modal-search.jsx
@@ -1,100 +1,114 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Backdrop from '@mui/material/Backdrop';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { useSpring, animated } from '@react-spring/web';
+import * as React from "react";
+import PropTypes from "prop-types";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useSpring, animated } from "@react-spring/web";
 import Search from "../images/search.svg?react";
 
 const Fade = React.forwardRef(function Fade(props, ref) {
-  const {
-    children,
-    in: open,
-    onClick,
-    onEnter,
-    onExited,
-    ownerState,
-    ...other
-  } = props;
-  const style = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: open ? 1 : 0 },
-    onStart: () => {
-      if (open && onEnter) {
-        onEnter(null, true);
-      }
-    },
-    onRest: () => {
-      if (!open && onExited) {
-        onExited(null, true);
-      }
-    },
-  });
+   const {
+      children,
+      in: open,
+      onClick,
+      onEnter,
+      onExited,
+      ownerState,
+      ...other
+   } = props;
+   const style = useSpring({
+      from: { opacity: 0 },
+      to: { opacity: open ? 1 : 0 },
+      onStart: () => {
+         if (open && onEnter) {
+            onEnter(null, true);
+         }
+      },
+      onRest: () => {
+         if (!open && onExited) {
+            onExited(null, true);
+         }
+      },
+   });
 
-  return (
-    <animated.div ref={ref} style={style} {...other}>
-      {React.cloneElement(children, { onClick })}
-    </animated.div>
-  );
+   return (
+      <animated.div ref={ref} style={style} {...other}>
+         {React.cloneElement(children, { onClick })}
+      </animated.div>
+   );
 });
 
 Fade.propTypes = {
-  children: PropTypes.element.isRequired,
-  in: PropTypes.bool,
-  onClick: PropTypes.any,
-  onEnter: PropTypes.func,
-  onExited: PropTypes.func,
-  ownerState: PropTypes.any,
+   children: PropTypes.element.isRequired,
+   in: PropTypes.bool,
+   onClick: PropTypes.any,
+   onEnter: PropTypes.func,
+   onExited: PropTypes.func,
+   ownerState: PropTypes.any,
 };
 
 const style = {
-  width: '80%', 
-  height:'50%', 
-  position: 'absolute',
-  top: '30%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
+   width: "80%",
+   height: "50%",
+   position: "absolute",
+   top: "30%",
+   left: "50%",
+   transform: "translate(-50%, -50%)",
+   bgcolor: "background.paper",
+   borderRadius: "1rem",
+   boxShadow: 24,
+   p: 4,
+   background: "var(--soft-background)",
 };
 
 export default function SpringModal() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+   const [open, setOpen] = React.useState(false);
+   const handleOpen = () => setOpen(true);
+   const handleClose = () => setOpen(false);
 
-  return (
-    <div>
-      <Button className="hover:rounded-full" onClick={handleOpen}><Search/></Button>
-      <Modal
-        aria-labelledby="spring-modal-title"
-        aria-describedby="spring-modal-description"
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        slots={{ backdrop: Backdrop }}
-        slotProps={{
-          backdrop: {
-            TransitionComponent: Fade,
-          },
-        }}
-      >
-        <Fade in={open}>
-          <Box sx={style}>
-            <Typography className=" bg-softBackground flex gap-4 " id="spring-modal-title" variant="h6" component="h2">
-              <input className=" bg-footerBackgroundSecondary ml-5 w-[70%] p-3 text-[0.875rem] duration-200 border-gray-300 shadow-input focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-300 focus:border-r-0 rounded-l-lg" type="text" />
-              <button className="">search</button>
-            </Typography>
-            <Typography id="spring-modal-description" sx={{ mt: 2 }}>
-              card
-            </Typography>
-          </Box>
-        </Fade>
-      </Modal>
-    </div>
-  );
+   return (
+      <div>
+         <Button className="hover:rounded-full" onClick={handleOpen}>
+            <Search />
+         </Button>
+         <Modal
+            aria-labelledby="spring-modal-title"
+            aria-describedby="spring-modal-description"
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            slots={{ backdrop: Backdrop }}
+            slotProps={{
+               backdrop: {
+                  TransitionComponent: Fade,
+               },
+            }}
+         >
+            <Fade in={open}>
+               <Box sx={style}>
+                  <Typography
+                     id="spring-modal-title"
+                     variant="h6"
+                     component="h2"
+                     className="flex gap-4"
+                  >
+                     <input
+                        className="text-2xl ml-5 w-[70%] p-3 text-[0.875rem] duration-200 border-gray-300 shadow-input focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-300 focus:border-r-0 rounded-lg"
+                        type="text"
+                     />
+                     <button className="">search</button>
+                  </Typography>
+                  <Typography
+                     id="spring-modal-description"
+                     sx={{ mt: 2 }}
+                  >
+                     card
+                  </Typography>
+               </Box>
+            </Fade>
+         </Modal>
+      </div>
+   );
 }
