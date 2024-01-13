3305da489ce1c4ecbff83de7934432447727479f
rasuljonadhamov <rasuljonadhamov@gmail.com>
2024-01-13 11:17:55 +0500

    #

--- src/pages/Home/BlogPostCard.jsx
+++ src/pages/Home/BlogPostCard.jsx
@@ -3,9 +3,9 @@ import arrow from "../../images/blog-posts/arrow-right.png";
 const BlogPostCard = ({ product }) => {
   const { id, image, name, description, postedAt } = product;
   return (
-    <div className="card w-72 ">
+    <div className="card w-72 " key={id}>
       <img src={image} alt={name} />
-      <div className="px-1.5 pb-4 bg-softBackground w-64 mx-auto">
+      <div className="px-1.5 pb-4 bg-softBackground w-64 ">
         <p className="mt-2 mb-1  text-primary">{postedAt}</p>
         <h3 className="mb-1 text-xl text">{name}</h3>
         <p className="text-sm mb-2 text-textSecondary">{description}</p>
