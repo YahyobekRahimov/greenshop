745f83d8cab995854c15571246a6699f321c6629
Yahyobek Rahimov <141851971+YahyobekRahimov@users.noreply.github.com>
2024-01-13 10:48:48 +0500

    removed bg of pictures, made some changes to the cards

--- src/pages/Home/Products.jsx
+++ src/pages/Home/Products.jsx
@@ -2,17 +2,21 @@ import Container from "../../components/Container";
 import data from "/data/data.json";
 
 export default function Products() {
-   let products = data.slice(44);
+   let products = data.slice(38);
    return (
       <Container>
          <h2 className="text-[2rem] text-center">Products</h2>
-         <div className="grid grid-cols-3 justify-items-center gap-y-16 pt-10 pb-10">
+         <div className="grid grid-cols-4 justify-items-center gap-y-16 pt-10 pb-10">
             {products.map((product) => (
                <div
                   key={product.id}
                   className="cursor-pointer shadow-custom w-[17rem] px-6 py-6 rounded-lg"
                >
-                  <img src={product.image} alt={product.name} />
+                  <img
+                     className="bg-softBackground"
+                     src={product.image}
+                     alt={product.name}
+                  />
                   <h3 className="text-[1.2rem]">{product.name}</h3>
                   <h4 className="font-bold text-primary text-[1.25rem]">
                      {product.price}
