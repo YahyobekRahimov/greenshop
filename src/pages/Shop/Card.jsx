60db12d01e5577ff5249a389d38c39f42b725a4f
UbaydulloH00 <turonarsloni00@gmail.com>
2024-01-13 15:29:11 +0500

    add trainsition for card

--- src/pages/Shop/Card.jsx
+++ src/pages/Shop/Card.jsx
@@ -1,20 +1,20 @@
 import Container from "../../components/Container";
 import data from '../../../data/data.json'
 import { useEffect } from "react";
+import SearchIcon from '../../images/searchicon.svg?react'
+import LikeIcon from '../../images/likeIcon.svg?react'
+import ShoppingIcon from '../../images/shoppingIcon.svg?react'
 
 export default function Products() {
    let products = data.slice(2);
-   useEffect(()=>{
-      alert('saliom')
-   })
    return (
       <Container>
          <h2 className="text-[2rem] text-center">Products</h2>
-         <div className="grid grid-cols-4 justify-items-center gap-y-16 pt-10 pb-10">
+         <div className="  grid grid-cols-4 justify-items-center gap-y-16 pt-10 pb-10">
             {products.map((product) => (
                <div
                   key={product.id}
-                  className="cursor-pointer shadow-custom w-[17rem] px-6 py-6 rounded-lg bg-softBackground"
+                  className=" cursor-pointer shadow-custom w-[17rem] px-6 py-6 rounded-lg bg-softBackground transition duration-300 transform hover:shadow-lg hover:scale-105"
                >
                   <img
                      className="w-[14rem] h-[14rem] object-cover mb-3"
@@ -27,6 +27,11 @@ export default function Products() {
                   <h4 className="font-bold text-primary text-[1.25rem]">
                      ${product.price}
                   </h4>
+                  <div className="px-5 flex gap-10 text-center ">
+                     <ShoppingIcon  />
+                     <LikeIcon/>
+                     <SearchIcon/>
+                  </div>
                </div>
             ))}
          </div>
