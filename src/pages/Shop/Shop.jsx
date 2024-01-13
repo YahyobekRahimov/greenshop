76b158bea84d228fb5dcbc1f0184e76a25ba6f64
UbaydulloH00 <turonarsloni00@gmail.com>
2024-01-13 16:03:57 +0500

    add hover

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,12 +1,12 @@
 import Container from "../../components/Container";
 import Card from "./Card";
 import Category from "./Category";
-import Tab from './Tab'
+
 
 export default function Shop() {
   return (
     <Container>
-    <Tab/>
+    
    <Category/>
    <Card/>
     </Container>
