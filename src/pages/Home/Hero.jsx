c39fa86d9b28617687d1b1696188d5b121af8241
Yahyobek Rahimov <141851971+YahyobekRahimov@users.noreply.github.com>
2024-01-13 10:00:07 +0500

    added carousel and cards

---
+++ src/pages/Home/Hero.jsx
@@ -0,0 +1,29 @@
+// Importing everything needed for the carousel
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+import Container from "../../components/Container";
+const images = [
+   {
+      original: "https://picsum.photos/id/1018/1000/600/",
+   },
+   {
+      original: "https://picsum.photos/id/1015/1000/600/",
+   },
+   {
+      original: "https://picsum.photos/id/1019/1000/600/",
+   },
+];
+
+export default function Hero() {
+   return (
+      <Container classes={`mt-3 mb-10`}>
+         <ImageGallery
+            items={images}
+            showThumbnails={false}
+            showPlayButton={false}
+            showFullscreenButton={false}
+            showBullets={true}
+         />
+      </Container>
+   );
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+import Products from "./Products";
+
+export default function Home() {
+   return (
+      <main>
+         <Hero />
+         <Products />
+      </main>
+   );
+}
