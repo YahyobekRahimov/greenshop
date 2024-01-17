67a6adc5fe2a689477a30d9dbdd79ce260c21fc6
UbaydulloH00 <turonarsloni00@gmail.com>
2024-01-17 15:10:56 +0500

    add card

--- src/pages/PlantCare.jsx
+++ src/pages/PlantCare.jsx
@@ -1,3 +1,11 @@
+import ProductDetails from "./ProductView";
+import Container from "../components/Container";
 export default function PlantCare() {
-   return <div>PlantCare</div>;
+   return (
+      <Container>
+         <div>
+            <ProductDetails />
+         </div>
+      </Container>
+   );
 }
