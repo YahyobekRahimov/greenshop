4606e73d19372d7c25748a269db51859b93c99df
Yahyobek Rahimov <141851971+YahyobekRahimov@users.noreply.github.com>
2024-01-20 17:57:15 +0500

    implemented input from mui to the login page

--- src/components/Header/LoginButtons.jsx
+++ src/components/Header/LoginButtons.jsx
@@ -3,9 +3,9 @@ import LogoutIcon from "../../images/Logout.svg?react";
 
 export default function LoginButton() {
    return (
-      <button className="hover:bg-primaryDark items-center flex gap-[0.25rem] text-white px-[1.0625rem] py-[0.5rem] rounded-md bg-green-600">
+      <div className="hover:bg-primaryDark items-center flex gap-[0.25rem] text-white px-[1.0625rem] py-[0.5rem] rounded-md bg-green-600">
          <LogoutIcon />
          Login
-      </button>
+      </div>
    );
 }
