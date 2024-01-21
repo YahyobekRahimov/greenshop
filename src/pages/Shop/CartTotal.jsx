7365753f144d868599447868189021c5718314e0
rasuljonadhamov <rasuljonadhamov@gmail.com>
2024-01-21 15:59:02 +0500

    added cart total section

--- src/pages/Shop/CartTotal.jsx
+++ src/pages/Shop/CartTotal.jsx
@@ -1,11 +1,12 @@
 import { TextField } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const CartTotal = () => {
   return (
-    <div>
-      <h1>Cart Totals </h1>
-      <span>Coupon Apply</span>
-      <div className="flex">
+    <div className="text-[0.938rem] text-textPrimary text-center">
+      <h1 className="text-left text-lg mb-5">Cart Totals </h1>
+      <span className="text-left text-sm mb-2 block">Coupon Apply</span>
+      <div className="flex mb-7">
         <TextField
           className="outline-primary -mr-1"
           id="outlined-basic"
@@ -16,6 +17,31 @@ const CartTotal = () => {
           Aapply
         </button>
       </div>
+      <div className="flex justify-between mb-4">
+        <span>Subtotal</span>
+        <span className="text-lg">$2,683.00</span>
+      </div>
+      <div className="flex justify-between mb-5">
+        <span>Coupon Discount</span>
+        <span>(-) 00.00</span>
+      </div>
+      <div className="flex justify-between mb-7">
+        <span>Shiping</span>
+        <div className="flex flex-col items-end">
+          <p className="text-lg mb-1">$16.00</p>
+          <p className="text-primary">View shipping charge</p>
+        </div>
+      </div>
+      <div className="flex justify-between mb-7">
+        <span className="text-base">Total</span>
+        <span>$2,699.00</span>
+      </div>
+      <button className="bg-primary py-3 px-20 mb-4 text-white rounded">
+        Proceed To Checkout
+      </button>
+      <Link className="text-primary" to="/shop">
+        Continue Shopping
+      </Link>
     </div>
   );
 };
