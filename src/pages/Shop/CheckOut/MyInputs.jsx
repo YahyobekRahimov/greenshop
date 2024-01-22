84c88330958daba64235dd255b93f9b7ca93d51e
rasuljonadhamov <rasuljonadhamov@gmail.com>
2024-01-22 16:10:04 +0500

    completed form section

--- src/pages/Shop/CheckOut/MyInputs.jsx
+++ src/pages/Shop/CheckOut/MyInputs.jsx
@@ -1,54 +1,64 @@
-function MyInputs({
+import { MenuItem, Select } from "@mui/material";
+import { React } from "react";
+
+const MyInputs = ({
   label = "",
-  name,
+  type = "text",
+  name = "",
   required,
   showSelect,
   placeholder = "",
   options,
   ...otherProps
-}) {
+}) => {
   if (showSelect) {
     const optionsArray = options.split(",").map((option) => option.trim());
 
     return (
       <div className="mb-4 w-[21.875rem] mr-6">
-        <label htmlFor={name} className="block text-gray-700 font-bold mb-2">
-          {label} {required && <span className="text-red">*</span>}
-        </label>
-        <select
-          className="w-full border border-gray-300 rounded-md px-3 py-2 leading-tight focus:outline-none focus:shadow-outline p-2"
-          name={name}
-          required={required}
-          {...otherProps}
-        >
-          {optionsArray.map((option) => (
-            <option key={option} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
+        <form>
+          <label htmlFor={name} className="block text-gray-700 font-bold mb-2">
+            {label} {required && <span className="text-red-700">*</span>}
+          </label>
+          <Select
+            value={name}
+            size="small"
+            className="w-full "
+            name={name}
+            required={required}
+            {...otherProps}
+          >
+            {optionsArray.map((option) => (
+              <MenuItem key={option} value={option}>
+                {option}
+              </MenuItem>
+            ))}
+          </Select>
+        </form>
       </div>
     );
   }
 
   return (
     <div className="mb-4 w-[21.875rem] mr-6">
-      <label htmlFor={name} className="block text-gray-700 font-bold mb-2">
-        {label} {required && <span className="text-red">*</span>}
-      </label>
-      <input
-        id={name}
-        type="text"
-        name={name}
-        required={required}
-        placeholder={placeholder}
-        className={`border border-gray-300 p-2 w-full rounded-md ${
-          required && !otherProps.value ? "border-x-gray-500" : ""
-        }`}
-        {...otherProps}
-      />
+      <form>
+        <label htmlFor={name} className="block text-gray-700 font-bold mb-2">
+          {label} {required && <span className="text-red-600">*</span>}
+        </label>
+        <input
+          id={name}
+          type={type}
+          name={name}
+          required={required}
+          placeholder={placeholder}
+          className={`border border-gray-300 p-2 w-full rounded-md ${
+            required && !otherProps.value ? "border-x-gray-500" : ""
+          }`}
+          {...otherProps}
+        />
+      </form>
     </div>
   );
-}
+};
 
 export default MyInputs;
--- src/pages/Shop/CheckOut/ProductCheckout.jsx
+++ src/pages/Shop/CheckOut/ProductCheckout.jsx
@@ -1,74 +1,11 @@
 import Container from "../../../components/Container";
-import MyInputs from "./MyInputs";
+import FormInputs from "./FormInputs";
 
 export default function ProductCheckout() {
   return (
     <Container>
       <div className="flex mb-52">
-        <div className="left">
-          <h1>Billing Address</h1>
-          <form>
-            <div className="w-[46.875rem] flex flex-wrap">
-              <MyInputs label="First Name" name="firstName" required={true} />
-              <MyInputs label="Last Name" name="lastName" required={true} />
-              <MyInputs
-                label="Country / Region"
-                name="country"
-                showSelect={true}
-                required={true}
-                options="Select a country / region, Uzbekistan, Russia"
-              />
-              <MyInputs label="Town / City" name="country" required={true} />
-              <MyInputs
-                label="Street Address"
-                placeholder="House number and street name"
-                name="country"
-                required={true}
-              />
-              <MyInputs
-                label=" ."
-                placeholder="Appartment, suite, unit, etc. (optional)"
-                name="country"
-                required={false}
-              />
-              <MyInputs
-                label="State"
-                name="state"
-                showSelect={true}
-                required={true}
-                options="Select a state, Uzbekistan, Russia"
-              />
-              <MyInputs label="Zip" placeholder="" name="zip" required={true} />
-              <MyInputs
-                label="Email address"
-                placeholder=""
-                name="zip"
-                required={true}
-              />
-              <MyInputs
-                label="Phone Number"
-                placeholder=""
-                name="phone"
-                required={true}
-              />
-              <div className="flex mb-11 w-[46.875rem]">
-                <input type="checkbox" />
-                <span className="block">Ship to a different address?</span>
-              </div>
-
-              <div className="block">
-                <p className="mb-2">Order notes (optional)</p>
-                <textarea
-                  className="border-2"
-                  name="text"
-                  id="text"
-                  cols="40"
-                  rows="5"
-                ></textarea>
-              </div>
-            </div>
-          </form>
-        </div>
+        <FormInputs />
       </div>
     </Container>
   );
