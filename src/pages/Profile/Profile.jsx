8c9316bd74fe30cc6f3c0af7d0542cc79a47283d
Yahyobek Rahimov <141851971+YahyobekRahimov@users.noreply.github.com>
2024-01-28 12:18:36 +0500

    tabs working in the profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,14 +1,24 @@
 import Container from "../../components/Container";
 import MyAccount from "./MyAccount/MyAccount";
 import Personalnfo from "./Personalnfo/Personalnfo";
+import { useState } from "react";
 
 export default function Profile() {
-  return (
-    <Container>
-      <div className="pt-[40px] mb-40 flex gap-7">
-        <MyAccount />
-        <Personalnfo />
-      </div>
-    </Container>
-  );
+   const [selectedTab, setSelectedTab] = useState(0);
+
+   let resultTab;
+   if (selectedTab == 0) {
+      resultTab = <Personalnfo />;
+   }
+   return (
+      <Container>
+         <div className="pt-[40px] mb-40 flex gap-7">
+            <MyAccount
+               selectedTab={selectedTab}
+               setSelectedTab={setSelectedTab}
+            />
+            {resultTab}
+         </div>
+      </Container>
+   );
 }
