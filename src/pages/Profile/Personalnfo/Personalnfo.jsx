1148341203d6a278ba0fa03af0f85cc1121a6875
Yahyobek Rahimov <141851971+YahyobekRahimov@users.noreply.github.com>
2024-01-30 17:28:12 +0500

    fixed myInput

--- src/pages/Profile/Personalnfo/Personalnfo.jsx
+++ src/pages/Profile/Personalnfo/Personalnfo.jsx
@@ -2,40 +2,46 @@ import MyInputs from "../../Shop/CheckOut/MyInputs";
 import { FormControl } from "@mui/material";
 
 const Personalnfo = () => {
-  return (
-    <div>
-      <p>Personal Information</p>
-      <FormControl>
-        <div className="w-[46.875rem] flex flex-wrap">
-          <MyInputs label="First Name" name="firstName" required={true} />
-          <MyInputs label="Last Name" name="lastName" required={true} />
-          <MyInputs
-            label="Email address"
-            placeholder=""
-            name="zip"
-            required={true}
-          />
-          <MyInputs
-            label="Phone Number"
-            placeholder=""
-            name="phone"
-            type="number"
-            required={true}
-          />
-          <MyInputs
-            label="Username"
-            placeholder=""
-            name="username"
-            required={true}
-          />
-          <MyInputs
-            label="Photo"
-            text= "true"
-          />
-        </div>
-      </FormControl>
-    </div>
-  );
+   return (
+      <div className="w-full">
+         <p>Personal Information</p>
+         <FormControl fullWidth>
+            <div className="w-full grid grid-cols-2 gap-x-[2rem]">
+               <MyInputs
+                  label="First Name"
+                  name="firstName"
+                  required={true}
+                  className="w-full"
+               />
+               <MyInputs
+                  label="Last Name"
+                  name="lastName"
+                  required={true}
+               />
+               <MyInputs
+                  label="Email address"
+                  placeholder=""
+                  name="zip"
+                  required={true}
+               />
+               <MyInputs
+                  label="Phone Number"
+                  placeholder=""
+                  name="phone"
+                  type="number"
+                  required={true}
+               />
+               <MyInputs
+                  label="Username"
+                  placeholder=""
+                  name="username"
+                  required={true}
+               />
+               <MyInputs label="Photo" text="true" />
+            </div>
+         </FormControl>
+      </div>
+   );
 };
 
 export default Personalnfo;
