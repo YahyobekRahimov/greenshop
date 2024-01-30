3d2f0ac889b9ce06818f75c329ee4b00c58f5a04
rasuljonadhamov <rasuljonadhamov@gmail.com>
2024-01-30 17:48:18 +0500

    finished personal info page styles

--- src/pages/Profile/Personalnfo/Personalnfo.jsx
+++ src/pages/Profile/Personalnfo/Personalnfo.jsx
@@ -1,47 +1,73 @@
 import MyInputs from "../../Shop/CheckOut/MyInputs";
-import { FormControl } from "@mui/material";
+import { Button, FormControl } from "@mui/material";
 
 const Personalnfo = () => {
-   return (
-      <div className="w-full">
-         <p>Personal Information</p>
-         <FormControl fullWidth>
-            <div className="w-full grid grid-cols-2 gap-x-[2rem]">
-               <MyInputs
-                  label="First Name"
-                  name="firstName"
-                  required={true}
-                  className="w-full"
-               />
-               <MyInputs
-                  label="Last Name"
-                  name="lastName"
-                  required={true}
-               />
-               <MyInputs
-                  label="Email address"
-                  placeholder=""
-                  name="zip"
-                  required={true}
-               />
-               <MyInputs
-                  label="Phone Number"
-                  placeholder=""
-                  name="phone"
-                  type="number"
-                  required={true}
-               />
-               <MyInputs
-                  label="Username"
-                  placeholder=""
-                  name="username"
-                  required={true}
-               />
-               <MyInputs label="Photo" text="true" />
-            </div>
-         </FormControl>
-      </div>
-   );
+  return (
+    <div className="w-full">
+      <p>Personal Information</p>
+      <FormControl fullWidth>
+        <div className="w-full grid grid-cols-2 gap-y-5 gap-x-[2rem]">
+          <MyInputs
+            label="First Name"
+            name="firstName"
+            required={true}
+            className="w-full"
+          />
+          <MyInputs label="Last Name" name="lastName" required={true} />
+          <MyInputs
+            label="Email address"
+            placeholder=""
+            name="zip"
+            required={true}
+          />
+          <MyInputs
+            label="Phone Number"
+            placeholder=""
+            name="phone"
+            type="number"
+            required={true}
+          />
+          <MyInputs
+            label="Username"
+            placeholder=""
+            name="username"
+            required={true}
+          />
+          <MyInputs label="Photo" text="true" />
+          <div className="flex flex-col gap-4">
+            <h2 className="w-full mb-2 mt-3">Password Change</h2>
+            <MyInputs
+              label="Current password"
+              name="password"
+              type="password"
+              required={true}
+            />
+
+            <MyInputs
+              label="New password"
+              name="password"
+              type="password"
+              required={true}
+            />
+
+            <MyInputs
+              label="Confirm new password"
+              name="password"
+              type="password"
+              required={true}
+            />
+
+            <Button
+              variant="contained"
+              style={{ width: "160px", fontSize: "14px" }}
+            >
+              Save Change
+            </Button>
+          </div>
+        </div>
+      </FormControl>
+    </div>
+  );
 };
 
 export default Personalnfo;
