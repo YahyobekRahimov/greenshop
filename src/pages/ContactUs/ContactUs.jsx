7f19ed63a95cf82cb30e3cdb126c31af9157ba52
rasuljonadhamov <rasuljonadhamov@gmail.com>
2024-02-01 16:58:47 +0500

    created contact page

---
+++ src/pages/ContactUs/ContactUs.jsx
@@ -0,0 +1,11 @@
+import Container from "../../components/Container";
+
+const ContactUs = () => {
+  return (
+    <Container>
+      <h1 className="text-[5.625rem] mx-auto text-[#140d01]">Contacts</h1>
+    </Container>
+  );
+};
+
+export default ContactUs;
