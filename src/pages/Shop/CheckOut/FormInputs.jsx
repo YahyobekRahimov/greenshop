fbdc41f248647b97b8bbbad825dec0bd8ccd563b
Yahyobek Rahimov <141851971+YahyobekRahimov@users.noreply.github.com>
2024-02-06 17:32:27 +0500

    fixed radio buttons, and form styles in checkout

--- src/pages/Shop/CheckOut/FormInputs.jsx
+++ src/pages/Shop/CheckOut/FormInputs.jsx
@@ -1,76 +1,96 @@
+import { Checkbox, FormControlLabel } from "@mui/material";
 import MyInputs from "./MyInputs.jsx";
 
 const FormInputs = () => {
-  return (
-    <div className="left mt-9 ">
-      <h1 className="mb-4">Billing Address</h1>
-      <form>
-        <div className="w-[46.875rem] flex flex-wrap">
-          <MyInputs label="First Name" name="firstName" required={true} />
-          <MyInputs label="Last Name" name="lastName" required={true} />
-          <MyInputs
-            label="Country / Region"
-            name="country"
-            showSelect={true}
-            required={true}
-            options="Select a country / region, Uzbekistan, Russia"
-          />
-          <MyInputs label="Town / City" name="country" required={true} />
-          <MyInputs
-            label="Street Address"
-            placeholder="House number and street name"
-            name="country"
-            required={true}
-          />
-          <MyInputs
-            label="Appartment"
-            placeholder="Appartment, suite, unit, etc. (optional)"
-            name="appartment"
-            required={false}
-          />
-          <MyInputs
-            label="State"
-            name="state"
-            showSelect={true}
-            required={true}
-            options="Select a state, Uzbekistan, Russia"
-          />
-          <MyInputs label="Zip" placeholder="" name="zip" required={true} />
-          <MyInputs
-            label="Email address"
-            placeholder=""
-            name="zip"
-            required={true}
-          />
-          <MyInputs
-            label="Phone Number"
-            placeholder=""
-            name="phone"
-            type="number"
-            required={true}
-          />
-          <div className="flex mb-11 w-[46.875rem]">
-            <input
-              type="checkbox"
-              className="rounded-3xl mr-2 outline-primary"
-            />
-            <span className="block">Ship to a different address?</span>
-          </div>
+   return (
+      <div className="left mt-9 ">
+         <h1 className="mb-4">Billing Address</h1>
+         <form>
+            <div className="w-[46.875rem] grid grid-cols-2 gap-x-8 gap-y-4">
+               <MyInputs
+                  label="First Name"
+                  name="firstName"
+                  required={true}
+               />
+               <MyInputs
+                  label="Last Name"
+                  name="lastName"
+                  required={true}
+               />
+               <MyInputs
+                  label="Country / Region"
+                  name="country"
+                  showSelect={true}
+                  required={true}
+                  options="Select a country / region, Uzbekistan, Russia"
+               />
+               <MyInputs
+                  label="Town / City"
+                  name="country"
+                  required={true}
+               />
+               <MyInputs
+                  label="Street Address"
+                  placeholder="House number and street name"
+                  name="country"
+                  required={true}
+               />
+               <MyInputs
+                  label="Appartment"
+                  placeholder="Appartment, suite, unit, etc. (optional)"
+                  name="appartment"
+                  required={false}
+               />
+               <MyInputs
+                  label="State"
+                  name="state"
+                  showSelect={true}
+                  required={true}
+                  options="Select a state, Uzbekistan, Russia"
+               />
+               <MyInputs
+                  label="Zip"
+                  placeholder=""
+                  name="zip"
+                  required={true}
+               />
+               <MyInputs
+                  label="Email address"
+                  placeholder=""
+                  name="zip"
+                  required={true}
+               />
+               <MyInputs
+                  label="Phone Number"
+                  placeholder=""
+                  name="phone"
+                  type="number"
+                  required={true}
+               />
+               <div>
+                  <div className="flex mb-11 w-full">
+                     <FormControlLabel
+                        control={<Checkbox />}
+                        label="Ship to a different address?"
+                        sx={{ userSelect: "none" }}
+                     ></FormControlLabel>
+                  </div>
 
-          <div className="block">
-            <p className="mb-2">Order notes (optional)</p>
-            <textarea
-              className="border-2 resize-none"
-              name="text"
-              id="text"
-              cols="40"
-              rows="5"
-            ></textarea>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
+                  <div className="block">
+                     <p className="mb-2">Order notes (optional)</p>
+                     <textarea
+                        className="resize-none p-3 border border-gray-300 p-2 w-full focus:border-blue-500 focus:outline-none focus:ring-blue-200 focus:ring-2 duration-200 rounded-md"
+                        name="text"
+                        id="text"
+                        cols="40"
+                        rows="5"
+                     ></textarea>
+                  </div>
+               </div>
+            </div>
+         </form>
+      </div>
+   );
 };
 
 export default FormInputs;
--- src/pages/Shop/CheckOut/Payment.jsx
+++ src/pages/Shop/CheckOut/Payment.jsx
@@ -1,36 +1,79 @@
-import { Checkbox } from "@mui/material";
+import {
+   Checkbox,
+   FormControlLabel as FormControlLabelBase,
+   Radio,
+   RadioGroup,
+} from "@mui/material";
 import imgPayment from "/src/images/payment.png";
 import OrderTrack from "../OrderTrack/OrderTrack";
 import { useState } from "react";
+import { styled } from "@mui/material/styles";
+
+const FormControlLabel = styled(FormControlLabelBase)`
+   border: 1px solid #eaeaea;
+   padding: 5px;
+`;
 
 const Payment = () => {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  return (
-    <div className="w-[25.313rem]">
-      <h1 className="mb-5">Payment Method</h1>
-      <div className="wrpapper border-2 pt-3 pl-3 pb-2 flex gap-2 w-80 mb-4">
-        <Checkbox />
-        <img src={imgPayment} className="w-56 h-6" alt="" />
+   const [open, setOpen] = useState(false);
+   const handleOpen = () => setOpen(true);
+   const handleClose = () => setOpen(false);
+   return (
+      <div className="w-[25.313rem]">
+         <h1 className="mb-5">Payment Method</h1>
+         <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="female"
+            name="radio-buttons-group"
+            sx={{
+               display: "flex",
+               flexDirection: "column",
+               gap: "1rem",
+            }}
+         >
+            <FormControlLabel
+               value="female"
+               control={<Radio />}
+               label={
+                  <img src={imgPayment} className="w-56 h-6" alt="" />
+               }
+            />
+            <FormControlLabel
+               value="bank"
+               control={<Radio />}
+               label="Direct bank transfer"
+            />
+            <FormControlLabel
+               value="cash"
+               control={<Radio />}
+               label="Cash on delivery"
+            />
+         </RadioGroup>
+         {/* <div className="wrpapper border-2 pt-3 pl-3 pb-2 flex gap-2 w-80 mb-4">
+            <Checkbox />
+            <img src={imgPayment} className="w-56 h-6" alt="" />
+         </div>
+         <div className="wrpapper border-2 pt-3 pl-3 pb-2 flex gap-2 w-80 mb-4">
+            <Checkbox />
+            <p>Dorect bank transfer</p>
+         </div>
+         <div className="wrpapper border-2 pt-3 pl-3 pb-2 flex gap-2 w-80 mb-4">
+            <Checkbox />
+            <p>Cash on delivery</p>
+         </div> */}
+         <button
+            onClick={handleOpen}
+            className="bg-primary w-80 text-white py-3 mt-10 rounded"
+         >
+            Place Order
+         </button>
+         <OrderTrack
+            open={open}
+            setOpen={setOpen}
+            handleClose={handleClose}
+         />
       </div>
-      <div className="wrpapper border-2 pt-3 pl-3 pb-2 flex gap-2 w-80 mb-4">
-        <Checkbox />
-        <p>Dorect bank transfer</p>
-      </div>
-      <div className="wrpapper border-2 pt-3 pl-3 pb-2 flex gap-2 w-80 mb-4">
-        <Checkbox />
-        <p>Cash on delivery</p>
-      </div>
-      <button
-        onClick={handleOpen}
-        className="bg-primary w-80 text-white py-3 mt-10 rounded"
-      >
-        Place Order
-      </button>
-      <OrderTrack open={open} setOpen={setOpen} handleClose={handleClose} />
-    </div>
-  );
+   );
 };
 
 export default Payment;
