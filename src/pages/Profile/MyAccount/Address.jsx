ba028cce11ce9789292acb252fd4cbf736b58391
Yahyobek Rahimov <141851971+YahyobekRahimov@users.noreply.github.com>
2024-02-07 06:07:58 +0500

    added select to the address page

--- src/pages/Profile/MyAccount/Address.jsx
+++ src/pages/Profile/MyAccount/Address.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import MyInputs from "../../Shop/CheckOut/MyInputs";
-import { FormControl } from "@mui/material";
-import Button from "../../../components/Button";
+import {
+   FormControl,
+   InputLabel,
+   MenuItem,
+   Select,
+} from "@mui/material";
+import { Button } from "@mui/material";
 import { useRef } from "react";
+import countries from "/data/countries.json";
 
 export default function Address() {
    const nameRef = useRef();
+   const countryRef = useRef();
    function clickButton() {
       console.log(nameRef.current.value);
    }
    return (
-      <div>
+      <div className="w-full">
          <div>
             <h1>Billing Address</h1>
             <p className="text-gray-500 mt-2 mb-7">
@@ -18,10 +25,9 @@ export default function Address() {
                page by default.
             </p>
          </div>
-         <FormControl>
-            <div className="w-[46.875rem] flex flex-wrap items-center">
+         <FormControl fullWidth>
+            <div className="grid grid-cols-2 w-full items-center gap-x-5 gap-y-3 mb-10">
                <MyInputs
-                 
                   label="First Name"
                   placeholder=""
                   name="firstName"
@@ -33,12 +39,28 @@ export default function Address() {
                   name="lastName"
                   required={true}
                />
-               <MyInputs
-                  label="Country / Region"
-                  placeholder="Select a country / region"
-                  name="country"
-                  required={true}
-               />
+               <FormControl fullWidth>
+                  <label
+                     htmlFor="country-region"
+                     className="mb-2 after:content-['*'] after:text-red-500 after:ml-1"
+                  >
+                     Country / region
+                  </label>
+                  <Select
+                     labelId="country-region"
+                     inputRef={countryRef}
+                     size="small"
+                  >
+                     {countries.map((country, index) => (
+                        <MenuItem
+                           key={index}
+                           value={country.toLowerCase()}
+                        >
+                           {country}
+                        </MenuItem>
+                     ))}
+                  </Select>
+               </FormControl>
                <MyInputs
                   label="Town / City"
                   required={true}
@@ -70,8 +92,9 @@ export default function Address() {
                />
             </div>
             <Button
-               onClick={clickButton}
-               classes=" hover:bg-primaryDark rounded w-[10rem] px-[1rem] py-[0.5rem] text-white bg-primary"
+               variant="contained"
+               className="w-max"
+               sx={{ paddingLeft: "3rem", paddingRight: "3rem" }}
             >
                Send
             </Button>
